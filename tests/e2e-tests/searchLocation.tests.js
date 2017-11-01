0a0db38c2b8a4fe5ea267023e8065a0c5ed439e4
Rafael Rocha <1140329@isep.ipp.pt>
2017-11-01 17:10:21 +0000

    Added some comments to the searchLocation test file.

--- tests/e2e-tests/searchLocation.tests.js
+++ tests/e2e-tests/searchLocation.tests.js
@@ -6,8 +6,9 @@ describe('Searching for a Location ', function(){
         addLocationButton = element(by.cssContainingText('.addLocation', 'Add Location'));
     });
 
-    it('should display the Search view, search for the inputed valid location and show its search results', function() {
+    it('should display the Search view, search for the inputed valid location, and show its search results', function() {
         addLocationButton.click().then(function() {
+            // check if it's the search page
             //expect(browser.getCurrentUrl()).toMatch('/search');
 
             var searchBar = element(by.id('searchbar'));
@@ -16,16 +17,19 @@ describe('Searching for a Location ', function(){
             var input = searchBar.element(by.css('.searchbar-input'));
             input.sendKeys('porto, portugal');
 
+            // wait for search results
             browser.sleep(15000);
 
+            // check if the correct result appears
             var searchResult = element(by.cssContainingText('.searchResult', 'Porto, Portugal'));
             expect(searchResult.isPresent()).toBe(true);
         });
     })
 
     
-    it('should display the Search view, search for the inputed invalid location and not show any search results', function() {
+    it('should display the Search view, search for the inputed invalid location, and not show any search results', function() {
         addLocationButton.click().then(function() {
+            // check if it's the search page
             //expect(browser.getCurrentUrl()).toMatch('/search');
 
             var searchBar = element(by.id('searchbar'));
@@ -34,8 +38,10 @@ describe('Searching for a Location ', function(){
             var input = searchBar.element(by.css('.searchbar-input'));
             input.sendKeys('aaaaaaaa');
 
+            // wait for search results
             browser.sleep(15000);
 
+            // check if any result appears
             var searchResult = element(by.css('.searchResult'));
             expect(searchResult.isPresent()).toBe(false);
         });
