7a77d6182b2ea7d25fcc15a9a19b2f312fa798b8
Rafa-Rocha <up201708900@fe.up.pt>
2017-12-10 16:18:13 +0000

    Added a backup key for WorldTides.

--- src/services/worldTides.service.ts
+++ src/services/worldTides.service.ts
@@ -10,13 +10,14 @@ export class WorldTidesService {
 
   // for WorldTides API
   private apiKey = 'c67a8277-8a61-43e5-9a38-db79eca54371';
+  private backupApiKey = "ce3a700d-80e0-4ffa-979f-c5d36f014bec";
   private baseUrl = 'https://www.worldtides.info/api?';
   private url;
 
   constructor(private storage: Storage, private dataService: DataService) { }
 
   private buildUrl(lat: string, lng: string,) {
-    this.url = this.baseUrl + 'heights&datum=LAT&lat='+ lat + '&lon=' + lng + '&key=' + this.apiKey;
+    this.url = this.baseUrl + 'heights&datum=LAT&lat='+ lat + '&lon=' + lng + '&key=' + this.backupApiKey;
     return this.url;
   }
 
