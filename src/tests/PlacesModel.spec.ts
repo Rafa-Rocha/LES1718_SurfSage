c04d647a3493ecf71dfffad9cb7264e5039f50eb
guifeliper <guifeliper@gmail.com>
2017-12-10 18:33:37 +0000

    Updating the package-json for Device running

--- src/tests/PlacesModel.spec.ts
+++ src/tests/PlacesModel.spec.ts
@@ -8,36 +8,36 @@ import { WUndergroundService } from '../services/wUnderground.service';
 import { WorldTidesService } from '../services/worldTides.service';
 import { Storage } from "@ionic/storage";
 
-describe('Places Models' , ()=> {
+describe('Models', () => {
     it('Create Place', () => {
-    
-      
-        var location = new Places('Country','Country', 'Latitude', 'Longitude');
-    
-        
-       expect(location.city).toEqual('Country');
-       expect(location.country).toEqual('Country');
-       expect(location.lat).toEqual('Latitude');
-       expect(location.lng).toEqual('Longitude');
- 
-        
-    
+
+
+        var location = new Places('Country', 'Country', 'Latitude', 'Longitude');
+
+
+        expect(location.city).toEqual('Country');
+        expect(location.country).toEqual('Country');
+        expect(location.lat).toEqual('Latitude');
+        expect(location.lng).toEqual('Longitude');
+
+
+
     })
 
     it('Create Weather', () => {
-        
-      
-            var weatherArray = new Weather(1,'100ºC', 'icon.png');
-        
-        
-           expect(weatherArray.id).toEqual(1);
-           expect(weatherArray.temperature).toEqual('100ºC');
-           expect(weatherArray.weatherIconURL).toEqual('icon.png');
-      
-     
-            
-        
-        })
-    
-    
+
+
+        var weatherArray = new Weather(1, '100ºC', 'icon.png');
+
+
+        expect(weatherArray.id).toEqual(1);
+        expect(weatherArray.temperature).toEqual('100ºC');
+        expect(weatherArray.weatherIconURL).toEqual('icon.png');
+
+
+
+
     })
+
+
+})
